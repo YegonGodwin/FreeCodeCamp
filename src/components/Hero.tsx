d29bed6a0de43531c501e7f224087ceabc9ef8f7
Godwin Kibet <kibetgodwin13@gmail.com>
2025-02-26 21:18:39 +0300

    Code optimization

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+export function Hero() {
+  return (
+    <section>
+      <img src="../assets/react.svg" />
+      <h2>Hello React developer</h2>
+    </section>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
 import { createRoot } from "react-dom/client";
+import { Hero } from "./components/Hero";
 
 const root = createRoot(document.getElementById("root")!);
 
+function NavBar() {
+  return (
+    <section>
+      <ul>
+        <li>Pricing</li>
+        <li>About Us</li>
+        <li>Contact</li>
+      </ul>
+    </section>
+  );
+}
+
 function App() {
-  return <h2>Hello React developer</h2>;
+  return (
+    <section>
+      <Hero />
+      <NavBar />
+      <h2>Fun fact about React</h2>;
+    </section>
+  );
 }
 root.render(<App />);
